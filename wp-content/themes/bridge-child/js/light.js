b456f744392a8eae1c6022bfdeed3f32a57976ba
Darkxee <app.chittrakorn@gmail.com>
2016-06-28 17:51:31 +0700

    js update

--- wp-content/themes/bridge-child/js/light.js
+++ wp-content/themes/bridge-child/js/light.js
@@ -1,27 +1,22 @@
 (function($) {
-    $.fn.light = function() {
 
-    };
     var mystone = {
-        slideindex: function(
-            a = 'li.active-revslide',
-            b = '#rev_slider_1_1 ul > li'
-        ) {
+        slideindex: function(a,b) {
+			a = a || 'li.active-revslide';
+            b = b || '#rev_slider_1_1 ul > li';
             return $(a).index(b);
         },
-        numslide: function(
-            a = '#rev_slider_1_1 ul > li'
-        ) {
+        numslide: function(a) {
+			a = a || '#rev_slider_1_1 ul > li';
             return $(a).size();
         },
 
-        btn_bullet_width: function(
-            a = '.tp-bullets'
-        ) {
+        btn_bullet_width: function(a) {
+			a = a || '.tp-bullets';
             return $(a).width();
         },
         bullet_lenth: function() {
-            var l = 0
+            var l = 0 ;
             var i = this.slideindex();
             if (this.slideindex() < 3) {
                 i = 0;
@@ -32,10 +27,9 @@
             }
             return l -= (this.btn_bullet_width() / this.numslide() * i);
         },
-        bullet_move: function(
-            a = this.bullet_lenth(),
-            b = '.overflow-bullet .btn-bullet'
-        ) {
+        bullet_move: function(a,b) {
+			a = a || this.bullet_lenth();
+            b = b || '.overflow-bullet .btn-bullet';
             $(b).animate({
                 'left': a
             }, 1000, function() {
@@ -58,7 +52,7 @@
     }
     $(document).ready(function() {
         if ($('body.page-id-24').length > 0) {
-            revapi1.bind("revolution.slide.onloaded", function(e) {
+            revapi1.bind("revolution.slide.onloaded", function() {
                 $('#rev_slider_1_1 .tp-bullets').wrapAll("<div class='overflow-bullet' />").wrapAll("<div class='btn-bullet'/>");
                 //.log("slider loaded");
                 $('.overflow-bullet .btn-bullet').width($('.overflow-bullet .tp-bullets').width());
@@ -67,7 +61,7 @@
             });
 
             // Slide change function
-            revapi1.bind("revolution.slide.onafterswap", function(e) {
+            revapi1.bind("revolution.slide.onafterswap", function() {
 
                 mystone.bullet_move(mystone.bullet_lenth());
                 // arrow_move();
@@ -92,11 +86,7 @@
     					.addTo(controller);
 
     });
-	function light(){
 
-	}
-
-	//light ccinneesrerte
 
 
 })(jQuery);
